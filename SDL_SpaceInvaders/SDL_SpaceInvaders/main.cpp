829d28f8940877330f4173a2fe9ea685182299b7
Adria-F <adriafc@TTC.CAT>
2017-03-07 12:34:10 +0100

    update 03/07/17

--- SDL_SpaceInvaders/SDL_SpaceInvaders/main.cpp
+++ SDL_SpaceInvaders/SDL_SpaceInvaders/main.cpp
@@ -5,25 +5,28 @@
 
 using namespace std;
 
-#define Player_Speed 10
+#define Player_Speed 0.1
 #define Bullet_Speed 0.25 //1 sec to cross screen 0.25
-#define Enemy_Speed 0.1 //2.5 sec to cross screen 0.1
+#define Enemy_Speed 0.2 //2.5 sec to cross screen 0.1
 #define Spawn_Delay 1 //in seconds 1
 #define Max_Enemies 3
 #define Max_Lives 3
+#define Max_Bullets 2
 
 
 #pragma comment( lib, "SDL2.lib" )
 #pragma comment ( lib, "SDL2main.lib" )
 
 SDL_Event event;
-bool shot = false;
+bool shot[Max_Bullets];
 float delay = 0;
 int lives = Max_Lives;
 int points = 0;
 bool dead = false;
+bool m_up = false, m_down = false, m_left = false, m_right = false, space = false;
+float character_pos_y = 0, character_pos_x = 0;
 
-void inputs(bool* running, SDL_Rect* character, SDL_Rect* bullet, float* bullet_pos)
+void inputs(bool* running, SDL_Rect* character, SDL_Rect bullet[], float bullet_pos[])
 {
 	while (SDL_PollEvent(&event))
 	{
@@ -31,62 +34,102 @@ void inputs(bool* running, SDL_Rect* character, SDL_Rect* bullet, float* bullet_
 		{
 			*running = false;
 			break;
-		}
+		}	
 		if (event.type == SDL_KEYDOWN)
+		{
+			if (event.key.repeat == 0)
+			{
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_SPACE:
+					for (int i = 0; i < Max_Bullets; i++)
+					{
+						if (shot[i] == false)
+						{
+							shot[i] = true;
+							bullet[i].x = character->x;
+							bullet[i].y = character->y + 12;
+							bullet[i].w = 50;
+							bullet[i].h = 25;
+							bullet_pos[i] = bullet[i].x;
+							break;
+						}
+					}
+					break;
+				case SDLK_UP:
+					m_up = true;
+					break;
+				case SDLK_DOWN:
+					m_down = true;
+					break;
+				case SDLK_LEFT:
+					m_left = true;
+					break;
+				case SDLK_RIGHT:
+					m_right = true;
+					break;
+				}
+			}
+		}
+		if (event.type == SDL_KEYUP)
 		{
 			switch (event.key.keysym.sym)
 			{
-			case SDLK_SPACE:
-				if (shot == false)
-				{
-					shot = true;
-					bullet->x = character->x;
-					bullet->y = character->y + 12;
-					bullet->w = 50;
-					bullet->h = 25;
-					*bullet_pos = bullet->x;
-				}
-				break;
 			case SDLK_UP:
-				if (character->y > 30)
-				{
-					character->y -= Player_Speed;
-				}
+				m_up = false;
 				break;
 			case SDLK_DOWN:
-				if (character->y < 430)
-				{
-					character->y += Player_Speed;
-				}
+				m_down = false;
 				break;
 			case SDLK_LEFT:
-				if (character->x > 0)
-				{
-					character->x -= Player_Speed;
-				}
+				m_left = false;
 				break;
 			case SDLK_RIGHT:
-				if (character->x < 670)
-				{
-					character->x += Player_Speed;
-				}
+				m_right = false;
 				break;
 			}
 		}
 	}
 }
 
-void draw_bullet(SDL_Rect* bullet, float* bullet_pos, SDL_Surface* sprite, SDL_Surface* screen)
+void move_character(SDL_Rect* character)
 {
-	if (shot && bullet->x < 770)
+	if (m_up && character->y > 30)
 	{
-		*bullet_pos += Bullet_Speed;
-		bullet->x = *bullet_pos;
+		character_pos_y -= Player_Speed;
+		character->y = character_pos_y;
 	}
-	if (shot && bullet->x == 770) { shot = false; }
-	if (shot)
+	if (m_down && character->y < 430)
 	{
-		SDL_BlitSurface(sprite, NULL, screen, bullet);
+		character_pos_y += Player_Speed;
+		character->y = character_pos_y;
+	}
+	if (m_left && character->x > 0)
+	{
+		character_pos_x -= Player_Speed;
+		character->x = character_pos_x;
+	}
+	if (m_right && character->x < 670)
+	{
+		character_pos_x += Player_Speed;
+		character->x = character_pos_x;
+	}
+}
+
+void draw_bullet(SDL_Rect bullet[], float bullet_pos[], SDL_Surface* sprite, SDL_Surface* screen)
+{
+	for (int i = 0; i < Max_Bullets; i++)
+	{
+		if (shot[i] && bullet[i].x < 770)
+		{
+			bullet_pos[i] += Bullet_Speed;
+			bullet[i].x = bullet_pos[i];
+		}
+		if (shot[i] && bullet[i].x == 770) { shot[i] = false; }
+		if (shot[i])
+		{
+			SDL_BlitSurface(sprite, NULL, screen, &bullet[i]);
+		}
 	}
 }
 
@@ -151,17 +194,20 @@ void detect_end(SDL_Rect* enemy, bool* free_enemy)
 	}
 }
 
-void bullet_hit(SDL_Rect* enemy, bool* free_enemy, SDL_Rect* bullet)
+void bullet_hit(SDL_Rect* enemy, bool* free_enemy, SDL_Rect bullet[])
 {
 	for (int i = 0; i < Max_Enemies; i++)
 	{
-		if (free_enemy[i] == false)
+		for (int j = 0; j < Max_Bullets; j++)
 		{
-			if (shot && ((bullet->x + 50) > enemy[i].x && bullet->x < (enemy[i].x + 40)) && ((bullet->y + 25) > enemy[i].y && bullet->y < (enemy[i].y + 40)))
+			if (free_enemy[i] == false)
 			{
-				free_enemy[i] = true;
-				shot = false;
-				points += 5;
+				if (shot[j] && ((bullet[j].x + 50) > enemy[i].x && bullet[j].x < (enemy[i].x + 40)) && ((bullet[j].y + 25) > enemy[i].y && bullet[j].y < (enemy[i].y + 40)))
+				{
+					free_enemy[i] = true;
+					shot[j] = false;
+					points += 5;
+				}
 			}
 		}
 	}
@@ -228,6 +274,7 @@ int main(int argc, char* argv[])
 {
 	srand(time(NULL));
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_RENDERER_PRESENTVSYNC;
 
 	char c_point[7];
 	char score[35] = "Space Invaders      Points: ";
@@ -257,13 +304,20 @@ int main(int argc, char* argv[])
 	character.w = 50;
 	character.x = 0;
 	character.y = 215;
+	character_pos_y = character.y;
+	character_pos_x = character.x;
 
-	SDL_Rect bullet;
-	bullet.h = 25;
-	bullet.w = 50;
-	bullet.x = 0;
-	bullet.y = 0;
-	float bullet_pos = 0;
+	SDL_Rect bullet[Max_Bullets];
+	float bullet_pos[Max_Bullets];
+	for (int i = 0; i < Max_Bullets; i++)
+	{
+		bullet[i].h = 25;
+		bullet[i].w = 50;
+		bullet[i].x = 0;
+		bullet[i].y = 0;
+		bullet_pos[Max_Bullets];
+		shot[i] = false;
+	}
 
 	SDL_Rect enemy[Max_Enemies];
 	bool* free_enemy = (bool*)malloc(Max_Enemies * sizeof(bool));
@@ -299,19 +353,20 @@ int main(int argc, char* argv[])
 	{
 		SDL_BlitSurface(background, NULL, screen, &bckg);
 
-		timer(&spawn);
+		timer(&spawn); //comment to delet enemies
 
 		if (spawn)
 		{
 			create_enemy(&enemy[0], free_enemy, &enemy_pos[0]);
 		}
 
-		inputs(&running, &character, &bullet, &bullet_pos);
+		inputs(&running, &character, &bullet[0], &bullet_pos[0]);
+		move_character(&character);
 		if (dead == false)
 		{
 			detect_end(&enemy[0], free_enemy);
-			bullet_hit(&enemy[0], free_enemy, &bullet);
-			draw_bullet(&bullet, &bullet_pos, missile, screen);
+			bullet_hit(&enemy[0], free_enemy, &bullet[0]);
+			draw_bullet(&bullet[0], &bullet_pos[0], missile, screen);
 			draw_enemy(&enemy[0], free_enemy, &enemy_pos[0], ship2, screen);
 			SDL_BlitSurface(ship, NULL, screen, &character);
 			draw_lives(&heart[0], lives, screen);
